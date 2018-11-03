2265dc8b8b76fe8eea28f13a28ae12b5cb4fb3f4
Ivan Djuric <djuricivan43@gmail.com>
2018-11-03 23:46:12 +0100

    Completed the interfece theme and other issues!

--- onlineshoppingstore/src/main/java/net/idj/onlineshoppingstore/controller/PageController.java
+++ onlineshoppingstore/src/main/java/net/idj/onlineshoppingstore/controller/PageController.java
@@ -3,7 +3,6 @@ package net.idj.onlineshoppingstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,9 +11,27 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
+	
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
 	
 //	@RequestMapping(value="/test")
 //	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
@@ -26,14 +43,14 @@ public class PageController {
 //		return mv;
 //	}
 	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
-		if(greeting == null) {
-			greeting = "Hello there";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-	}
+//	@RequestMapping(value="/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting") String greeting) {
+//		if(greeting == null) {
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
 	
 }
---
+++ onlineshoppingstore/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,8 @@
+<div class ="container">
+	<div class = "rows">
+	
+		<h1>Contact Us</h1>
+		<hr/>
+		<p>This is contact us page </p>
+	</div>
+</div>
